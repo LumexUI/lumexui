491d4d3041b975dcca7dcd50f1e937ca4282497c
desmondinho <daniil.norinn@gmail.com>
2024-05-13 23:36:27 +0300

    feat(components): add the base component

---
+++ src/LumexUI/Components/Bases/LumexComponentBase.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) LumexUI 2024
+// LumexUI licenses this file to you under the MIT license
+// See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
+
+using Microsoft.AspNetCore.Components;
+
+namespace LumexUI;
+
+public abstract class LumexComponentBase : ComponentBase
+{
+	/// <summary>
+	/// Gets or sets an HTML tag of the component.
+	/// </summary>
+	[Parameter] public string As { get; set; } = "span";
+
+	/// <summary>
+	/// Gets or sets CSS class names that will be applied to the component.
+	/// </summary>
+	[Parameter] public string? Class { get; set; }
+
+	/// <summary>
+	/// Gets or sets styles that will be applied to the component.
+	/// </summary>
+	[Parameter] public string? Style { get; set; }
+
+	/// <summary>
+	/// Gets or sets a collection of additional attributes that will be applied to the component.
+	/// </summary>
+	[Parameter( CaptureUnmatchedValues = true )]
+	public IReadOnlyDictionary<string, object>? AdditionalAttributes { get; set; } = new Dictionary<string, object>();
+}
