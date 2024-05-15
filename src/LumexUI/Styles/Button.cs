beb6095483423e2e02148575cbdbb8a9aa3c46e3
desmondinho <daniil.norinn@gmail.com>
2024-05-15 19:38:53 +0300

    chore(components): exclude button styles from code coverage

--- src/LumexUI/Styles/Button.cs
+++ src/LumexUI/Styles/Button.cs
@@ -2,11 +2,14 @@
 // LumexUI licenses this file to you under the MIT license
 // See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
 
+using System.Diagnostics.CodeAnalysis;
+
 using LumexUI.Common;
 using LumexUI.Utilities;
 
 namespace LumexUI.Styles;
 
+[ExcludeFromCodeCoverage]
 internal readonly record struct Button
 {
     private static string _base = CssBuilder.Empty()
