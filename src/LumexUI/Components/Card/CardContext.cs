e67ed8b092c5961fecdcbde03a5bf83160641cba
desmondinho <daniil.norinn@gmail.com>
2024-05-23 22:21:55 +0300

    feat(components): add the card component

---
+++ src/LumexUI/Components/Card/CardContext.cs
@@ -0,0 +1,12 @@
+﻿namespace LumexUI;
+
+internal class CardContext( LumexCard owner )
+{
+    public LumexCard Owner { get; } = owner;
+
+    public static void ThrowMissingParentComponentException( string componentName )
+    {
+        throw new InvalidOperationException( 
+            $"<{componentName} /> component must be used within a <{nameof( LumexCard )} /> component." );
+    }
+}
---
+++ src/LumexUI/Components/Card/CardSlots.cs
@@ -0,0 +1,9 @@
+﻿namespace LumexUI;
+
+public class CardSlots
+{
+    public string? Base { get; set; }
+    public string? Header { get; set; }
+    public string? Body { get; set; }
+    public string? Footer { get; set; }
+}
