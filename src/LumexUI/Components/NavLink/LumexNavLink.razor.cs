64b3839564a9f344cdf6e5a900fe1999524b8b0e
desmondinho <daniil.norinn@gmail.com>
2024-07-05 23:47:00 -0500

    feat(components): add Disabled and Color parameters to the navlink component

--- src/LumexUI/Components/NavLink/LumexNavLink.razor.cs
+++ src/LumexUI/Components/NavLink/LumexNavLink.razor.cs
@@ -2,6 +2,8 @@
 // LumexUI licenses this file to you under the MIT license
 // See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
 
+using LumexUI.Common;
+
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Routing;
 
@@ -28,8 +30,26 @@ public partial class LumexNavLink : LumexComponentBase
     [Parameter] public NavLinkMatch Match { get; set; } = NavLinkMatch.All;
 
     /// <summary>
+    /// Gets or sets a color of the navigation link.
+    /// </summary>
+    /// <remarks>
+    /// Default is <see cref="ThemeColor.Primary"/>
+    /// </remarks>
+    [Parameter] public ThemeColor Color { get; set; } = ThemeColor.Primary;
+
+    /// <summary>
     /// Gets or sets the CSS class name applied to the navigation link 
     /// when the current route matches the <see cref="Href"/>.
     /// </summary>
     [Parameter] public string? ActiveClass { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the navigation link is disabled.
+    /// </summary>
+    [Parameter] public bool Disabled { get; set; }
+
+    public LumexNavLink()
+    {
+        As = "li";
+    }
 }
