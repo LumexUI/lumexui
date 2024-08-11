9b9ce36fec71326d9f70ed3922f5363c5a5cfd63
desmondinho <daniil.norinn@gmail.com>
2024-08-11 12:56:33 +0300

    chore(textbox): usings cleanup

--- src/LumexUI/Components/Textbox/LumexTextbox.razor.cs
+++ src/LumexUI/Components/Textbox/LumexTextbox.razor.cs
@@ -2,8 +2,6 @@
 // LumexUI licenses this file to you under the MIT license
 // See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
 
-using System.Diagnostics.CodeAnalysis;
-
 using LumexUI.Common;
 using LumexUI.Utilities;
 
