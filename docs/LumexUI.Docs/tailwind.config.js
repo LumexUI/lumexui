12d113dd69686d3a252872ac597085ff986b1636
desmondinho <daniil.norinn@gmail.com>
2024-10-20 20:49:11 +0300

    refactor(docs): move all projects to the 'docs' folder

--- docs/LumexUI.Docs/tailwind.config.js
+++ docs/LumexUI.Docs/tailwind.config.js
@@ -1,5 +1,5 @@
-const lumexui = '../../../src/LumexUI/';
 const defaultTheme = require('tailwindcss/defaultTheme');
+const lumexui = '../../src/LumexUI/';
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
