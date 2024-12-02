4353c492977c5dc6d33c4dc8b8f4a14df2351986
creanium <john@creanium.net>
2024-12-01 22:52:51 -0700

    More tests

--- tests/LumexUI.Tests/Components/RadioGroup/RadioGroupTests.cs
+++ tests/LumexUI.Tests/Components/RadioGroup/RadioGroupTests.cs
@@ -2,13 +2,15 @@
 // LumexUI licenses this file to you under the MIT license
 // See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
 
+using LumexUI.Common;
+
 using Microsoft.Extensions.DependencyInjection;
 
 using TailwindMerge;
 
 namespace LumexUI.Tests.Components;
 
-public class RadioGroupTests: TestContext
+public class RadioGroupTests : TestContext
 {
     public RadioGroupTests()
     {
@@ -18,17 +20,27 @@ public class RadioGroupTests: TestContext
     [Fact]
     public void RadioGroup_ShouldRenderCorrectly()
     {
-        var action = () => RenderComponent<LumexRadioGroup<string>>( p => p
+        var action = () => RenderComponent<LumexRadioGroup<string>>( g => g
             .Add( p => p.Label, "Select City" )
             .Add( p => p.Description, "Select the city in which you currently live" )
-            .AddChildContent<LumexRadio<string>>( p => p
+            .AddChildContent<LumexRadio<string>>( r => r
                 .Add( p => p.Value, "tallinn" )
                 .AddChildContent( "Tallinn" ) )
-            .AddChildContent<LumexRadio<string>>( p => p
+            .AddChildContent<LumexRadio<string>>( r => r
                 .Add( p => p.Value, "madrid" )
                 .AddChildContent( "Madrid" ) )
         );
 
         action.Should().NotThrow();
+
+        var cut = action.Invoke();
+        var radioButtons = cut.FindComponents<LumexRadio<string>>();
+
+        radioButtons.Count.Should().Be( 2 );
+
+        radioButtons[0].Instance.Value.Should().Be( "tallinn" );
+        radioButtons[0].Markup.Should().Contain( "Tallinn" );
+        radioButtons[1].Instance.Value.Should().Be( "madrid" );
+        radioButtons[1].Markup.Should().Contain( "Madrid" );
     }
 }
