4c97b834972355aed949c30459451f20a89d93a3
desmondinho <daniil.norinn@gmail.com>
2024-12-13 16:05:11 +0200

    feat: add `TextValue` param to the SelectItem component

--- src/LumexUI/Components/Select/LumexSelectItem.cs
+++ src/LumexUI/Components/Select/LumexSelectItem.cs
@@ -2,6 +2,8 @@
 // LumexUI licenses this file to you under the MIT license
 // See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
 
+using System.Diagnostics;
+
 using LumexUI.Common;
 
 using Microsoft.AspNetCore.Components;
@@ -9,16 +11,46 @@ using Microsoft.AspNetCore.Components;
 namespace LumexUI;
 
 /// <summary>
-/// 
+/// A component representing a selectable item within the <see cref="LumexSelect{TValue}"/>.
 /// </summary>
-/// <typeparam name="TValue"></typeparam>
-public class LumexSelectItem<TValue> : LumexListboxItem<TValue> 
+/// <typeparam name="TValue">The type of the value associated with the select item.</typeparam>
+public class LumexSelectItem<TValue> : LumexListboxItem<TValue>, IDisposable
 {
-    [CascadingParameter] internal new SelectContext<TValue> Context { get; set; } = default!;
+    /// <summary>
+    /// Gets or sets a text representation of the select item value.
+    /// </summary>
+    /// <remarks>
+    /// The default value is <see cref="LumexListboxItem{TValue}.Value"/>
+    /// </remarks>
+    [Parameter] public string? TextValue { get; set; }
+
+    [CascadingParameter] internal SelectContext<TValue> SelectContext { get; set; } = default!;
 
     /// <inheritdoc />
     protected override void OnInitialized()
     {
-        ContextNullException.ThrowIfNull( Context, nameof( LumexSelectItem<TValue> ) );
+        base.OnInitialized();
+
+        ContextNullException.ThrowIfNull( SelectContext, nameof( LumexSelectItem<TValue> ) );
+        SelectContext.Register( this );
+    }
+
+    /// <inheritdoc />
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+        Debug.Assert( Value is not null );
+
+        if( string.IsNullOrEmpty( TextValue ) )
+        {
+            TextValue = Value.ToString();
+        }
+    }
+
+    /// <inheritdoc />
+    public override void Dispose()
+    {
+        SelectContext.Unregister( this );
+        base.Dispose();
     }
 }
