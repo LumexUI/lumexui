178360efd90575901059b92a34ebf78c83a41439
desmondinho <daniil.norinn@gmail.com>
2025-02-01 18:29:46 +0200

    initial commit

---
+++ src/LumexUI/Components/Dropdown/LumexDropdown.razor.cs
@@ -0,0 +1,13 @@
+// Copyright (c) LumexUI 2024
+// LumexUI licenses this file to you under the MIT license
+// See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
+
+namespace LumexUI;
+
+/// <summary>
+/// 
+/// </summary>
+public partial class LumexDropdown : LumexComponentBase
+{
+
+}
