10b8b61401701e9658367f482215a7870e8b8bd0
desmondinho <daniil.norinn@gmail.com>
2025-02-05 02:40:34 +0200

    improve usage of the popover within the dropdown

--- src/LumexUI/Components/Dropdown/LumexDropdown.razor.cs
+++ src/LumexUI/Components/Dropdown/LumexDropdown.razor.cs
@@ -2,20 +2,13 @@
 // LumexUI licenses this file to you under the MIT license
 // See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
 
-using Microsoft.AspNetCore.Components;
-
 namespace LumexUI;
 
 /// <summary>
 /// 
 /// </summary>
-public partial class LumexDropdown : LumexComponentBase
+public partial class LumexDropdown : LumexPopover
 {
-	/// <summary>
-	/// 
-	/// </summary>
-	[Parameter] public RenderFragment? ChildContent { get; set; }
-
 	private readonly DropdownContext _context;
 
 	/// <summary>
