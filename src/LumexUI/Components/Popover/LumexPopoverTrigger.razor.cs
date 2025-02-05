66de3d8e75829eccfa534f816e0f9faafb5754b9
desmondinho <daniil.norinn@gmail.com>
2025-02-05 02:36:49 +0200

    refactor(popover): deprecate `LumexPopoverTrigger`

--- src/LumexUI/Components/Popover/LumexPopoverTrigger.razor.cs
+++ src/LumexUI/Components/Popover/LumexPopoverTrigger.razor.cs
@@ -15,79 +15,80 @@ namespace LumexUI;
 /// <summary>
 /// A component representing the trigger of the <see cref="LumexPopover"/> component, controlling its display.
 /// </summary>
+[Obsolete( "Deprecated. Thi will be removed in the future releases. See the Popover usage in the docs." )]
 [CompositionComponent( typeof( LumexPopover ) )]
 public partial class LumexPopoverTrigger : LumexButton
 {
-    [CascadingParameter] internal PopoverContext Context { get; set; } = default!;
+	[CascadingParameter] internal PopoverContext Context { get; set; } = default!;
 
-    private protected override string? RootClass =>
-        TwMerge.Merge( base.RootClass, Popover.GetTriggerStyles( this ) );
+	private protected override string? RootClass =>
+		TwMerge.Merge( base.RootClass, Popover.GetTriggerStyles( this ) );
 
-    /// <inheritdoc />
-    public override async Task SetParametersAsync( ParameterView parameters )
-    {
-        await base.SetParametersAsync( parameters );
+	/// <inheritdoc />
+	public override async Task SetParametersAsync( ParameterView parameters )
+	{
+		await base.SetParametersAsync( parameters );
 
-        Color = parameters.TryGetValue<ThemeColor>( nameof( Color ), out var color )
-            ? color
-            : Context.Owner.Color;
-    }
+		Color = parameters.TryGetValue<ThemeColor>( nameof( Color ), out var color )
+			? color
+			: Context.Owner.Color;
+	}
 
-    /// <inheritdoc />
-    protected override void OnInitialized()
-    {
-        ContextNullException.ThrowIfNull( Context, nameof( LumexPopoverTrigger ) );
+	/// <inheritdoc />
+	protected override void OnInitialized()
+	{
+		ContextNullException.ThrowIfNull( Context, nameof( LumexPopoverTrigger ) );
 
-        SetAdditionalAttributes();
-    }
+		SetAdditionalAttributes();
+	}
 
-    private void SetAdditionalAttributes()
-    {
-        if( TryConvertToDictionary( AdditionalAttributes, out var additionalAttributes ) )
-        {
-            AdditionalAttributes = additionalAttributes;
-        }
+	private void SetAdditionalAttributes()
+	{
+		if( TryConvertToDictionary( AdditionalAttributes, out var additionalAttributes ) )
+		{
+			AdditionalAttributes = additionalAttributes;
+		}
 
-        additionalAttributes["data-slot"] = "trigger";
-        additionalAttributes["data-popoverref"] = Context.Owner.Id;
-    }
+		additionalAttributes["data-slot"] = "trigger";
+		additionalAttributes["data-popoverref"] = Context.Owner.Id;
+	}
 
-    private protected override async Task OnClickAsync( MouseEventArgs args )
-    {
-        if( Disabled )
-        {
-            return;
-        }
+	private protected override async Task OnClickAsync( MouseEventArgs args )
+	{
+		if( Disabled )
+		{
+			return;
+		}
 
-        await Context.TriggerAsync();
-        await OnClick.InvokeAsync( args );
-    }
+		await Context.TriggerAsync();
+		await OnClick.InvokeAsync( args );
+	}
 
-    // TODO: Move to the extensions (duplicate of ConvertToDictionary in LumexNumbox)
-    [ExcludeFromCodeCoverage]
-    private static bool TryConvertToDictionary( IReadOnlyDictionary<string, object>? source, out Dictionary<string, object> result )
-    {
-        var newDictionaryCreated = true;
+	// TODO: Move to the extensions (duplicate of ConvertToDictionary in LumexNumbox)
+	[ExcludeFromCodeCoverage]
+	private static bool TryConvertToDictionary( IReadOnlyDictionary<string, object>? source, out Dictionary<string, object> result )
+	{
+		var newDictionaryCreated = true;
 
-        if( source is null )
-        {
-            result = [];
-        }
-        else if( source is Dictionary<string, object> currentDictionary )
-        {
-            result = currentDictionary;
-            newDictionaryCreated = false;
-        }
-        else
-        {
-            result = [];
+		if( source is null )
+		{
+			result = [];
+		}
+		else if( source is Dictionary<string, object> currentDictionary )
+		{
+			result = currentDictionary;
+			newDictionaryCreated = false;
+		}
+		else
+		{
+			result = [];
 
-            foreach( var item in source )
-            {
-                result.Add( item.Key, item.Value );
-            }
-        }
+			foreach( var item in source )
+			{
+				result.Add( item.Key, item.Value );
+			}
+		}
 
-        return newDictionaryCreated;
-    }
+		return newDictionaryCreated;
+	}
 }
