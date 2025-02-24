745b91e249fc69f07cd97079a9b4b6e9680d62d1
desmondinho <daniil.norinn@gmail.com>
2025-02-24 21:01:14 +0200

    feat: add baseline implementation

---
+++ src/LumexUI/Components/Avatar/LumexAvatar.razor.cs
@@ -0,0 +1,87 @@
+// Copyright (c) LumexUI 2024
+// LumexUI licenses this file to you under the MIT license
+// See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
+
+using LumexUI.Common;
+
+using Microsoft.AspNetCore.Components;
+
+namespace LumexUI;
+
+/// <summary>
+/// A component that represents an avatar, typically displaying a user's profile image or initials.
+/// </summary>
+public partial class LumexAvatar : LumexComponentBase
+{
+	/// <summary>
+	/// Gets or sets the content to render when the avatar image is unavailable.
+	/// </summary>
+	[Parameter] public RenderFragment? FallbackContent { get; set; }
+
+	/// <summary>
+	/// Gets or sets the URL of the avatar image.
+	/// </summary>
+	[Parameter] public string? Src { get; set; }
+
+	/// <summary>
+	/// Gets or sets the alternative text for the avatar image.
+	/// </summary>
+	/// <remarks>
+	/// The default value is <c>"avatar"</c>.
+	/// </remarks>
+	[Parameter] public string Alt { get; set; } = "avatar";
+
+	/// <summary>
+	/// Gets or sets the name associated with the avatar, 
+	/// used to generate initials if no image is provided.
+	/// </summary>
+	[Parameter] public string? Name { get; set; }
+
+	/// <summary>
+	/// Gets or sets the icon displayed when no image or initials are available.
+	/// </summary>
+	/// <remarks>
+	/// The default value is <see cref="Icons.Rounded.Person"/>.
+	/// </remarks>
+	[Parameter] public string Icon { get; set; } = Icons.Rounded.Person;
+
+	/// <summary>
+	/// Gets or sets the function that resolves initials from the provided name.
+	/// </summary>
+	[Parameter] public InitialsResolver Initials { get; set; }
+
+	private readonly RenderFragment _renderFallback;
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="LumexAvatar"/>.
+	/// </summary>
+	public LumexAvatar()
+	{
+		_renderFallback = RenderFallback;
+
+		As = "span";
+		Initials = ExtractInitials;
+	}
+
+	/// <inheritdoc />
+	protected override void OnParametersSet()
+	{
+		if( !string.IsNullOrWhiteSpace( Name ) )
+		{
+			Alt = Name;
+		}
+	}
+
+	private string ExtractInitials( string name )
+	{
+		var names = name.Split( ' ', StringSplitOptions.RemoveEmptyEntries );
+		return names.Length > 1
+			? $"{names[0][0]} {names[^1][0]}".ToUpper()
+			: ShortenIfNeeded( names[0] );
+
+		static string ShortenIfNeeded( string text )
+		{
+			return text.Length <= 4 ? text : text[0..3];
+		}
+	}
+}
