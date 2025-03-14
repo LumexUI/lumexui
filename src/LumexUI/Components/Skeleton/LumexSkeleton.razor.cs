8f58a14d6d68de0a11a33f483f1902a372f128d9
desmondinho <daniil.norinn@gmail.com>
2025-03-14 22:13:50 +0200

    feat(skeleton): add baseline implementation of the component

---
+++ src/LumexUI/Components/Skeleton/LumexSkeleton.razor.cs
@@ -0,0 +1,23 @@
+// Copyright (c) LumexUI 2024
+// LumexUI licenses this file to you under the MIT license
+// See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
+
+using Microsoft.AspNetCore.Components;
+
+namespace LumexUI;
+
+/// <summary>
+/// 
+/// </summary>
+public partial class LumexSkeleton : LumexComponentBase
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[Parameter] public RenderFragment? ChildContent { get; set; }
+
+	/// <summary>
+	/// 
+	/// </summary>
+	[Parameter] public bool Loading { get; set; }
+}
