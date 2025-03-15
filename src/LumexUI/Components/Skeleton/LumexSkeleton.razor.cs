76316e4c666bdb127418d8669f7368ff5b8c1584
desmondinho <daniil.norinn@gmail.com>
2025-03-15 15:16:45 +0200

    feat(skeleton): add slots and styles

--- src/LumexUI/Components/Skeleton/LumexSkeleton.razor.cs
+++ src/LumexUI/Components/Skeleton/LumexSkeleton.razor.cs
@@ -2,6 +2,11 @@
 // LumexUI licenses this file to you under the MIT license
 // See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
 
+using System.Diagnostics.CodeAnalysis;
+
+using LumexUI.Common;
+using LumexUI.Utilities;
+
 using Microsoft.AspNetCore.Components;
 
 namespace LumexUI;
@@ -9,7 +14,7 @@ namespace LumexUI;
 /// <summary>
 /// 
 /// </summary>
-public partial class LumexSkeleton : LumexComponentBase
+public partial class LumexSkeleton : LumexComponentBase, ISlotComponent<SkeletonSlots>
 {
 	/// <summary>
 	/// 
@@ -19,5 +24,38 @@ public partial class LumexSkeleton : LumexComponentBase
 	/// <summary>
 	/// 
 	/// </summary>
-	[Parameter] public bool Loading { get; set; }
+	/// <remarks>
+	/// The default value is <see langword="true"/>.
+	/// </remarks>
+	[Parameter] public bool Loading { get; set; } = true;
+
+	/// <summary>
+	/// 
+	/// </summary>
+	[Parameter] public SkeletonSlots? Classes { get; set; }
+
+	private Dictionary<string, ComponentSlot> _slots = [];
+
+	/// <inheritdoc />
+	protected override void OnParametersSet()
+	{
+		var skeleton = Styles.Skeleton.Style( TwMerge );
+		_slots = skeleton();
+	}
+
+	[ExcludeFromCodeCoverage]
+	private string? GetStyles( string slot )
+	{
+		if( !_slots.TryGetValue( slot, out var styles ) )
+		{
+			throw new NotImplementedException();
+		}
+
+		return slot switch
+		{
+			nameof( SkeletonSlots.Base ) => styles( Classes?.Base, Class ),
+			nameof( SkeletonSlots.Content ) => styles( Classes?.Content ),
+			_ => throw new NotImplementedException()
+		};
+	}
 }
