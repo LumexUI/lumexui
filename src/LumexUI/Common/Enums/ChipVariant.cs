2b16de37175f4909f17f7c363518c8c3bb9085ac
desmondinho <daniil.norinn@gmail.com>
2025-03-20 21:30:39 +0200

    feat(chip): add ChipVariant enum

---
+++ src/LumexUI/Common/Enums/ChipVariant.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) LumexUI 2024
+// LumexUI licenses this file to you under the MIT license
+// See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
+
+namespace LumexUI.Common;
+
+/// <summary>
+/// Specifies the visual variants for the <see cref="LumexChip"/>.
+/// </summary>
+public enum ChipVariant
+{
+	Solid,
+
+	Outlined,
+
+	Flat,
+
+	Shadow,
+
+	Light,
+
+	Dot
+}
