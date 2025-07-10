774757e095ca51b8cddeaa5326fede78dc23c011
desmondinho <daniil.norinn@gmail.com>
2025-07-10 19:02:21 +0300

    feat(badge): initial

---
+++ src/LumexUI/Components/Badge/LumexBadge.razor.cs
@@ -0,0 +1,13 @@
+// Copyright (c) LumexUI 2024
+// LumexUI licenses this file to you under the MIT license
+// See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
+
+namespace LumexUI;
+
+/// <summary>
+/// A component that represents a badge for displaying contextual information or status indicators.
+/// </summary>
+public partial class LumexBadge : LumexComponentBase
+{
+
+}
