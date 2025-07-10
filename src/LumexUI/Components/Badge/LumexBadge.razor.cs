f805e83de304dbd52c7678420602c5d2486bcc91
desmondinho <daniil.norinn@gmail.com>
2025-07-10 19:19:59 +0300

    feat(badge): add badge baseline implementation

--- src/LumexUI/Components/Badge/LumexBadge.razor.cs
+++ src/LumexUI/Components/Badge/LumexBadge.razor.cs
@@ -2,6 +2,8 @@
 // LumexUI licenses this file to you under the MIT license
 // See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
 
+using System.Diagnostics.CodeAnalysis;
+
 using LumexUI.Common;
 
 using Microsoft.AspNetCore.Components;
@@ -14,7 +16,41 @@ namespace LumexUI;
 public partial class LumexBadge : LumexComponentBase, ISlotComponent<BadgeSlots>
 {
 	/// <summary>
+	/// Gets or sets the content to render inside the badge.
+	/// </summary>
+	[Parameter] public RenderFragment? ChildContent { get; set; }
+
+	/// <summary>
+	/// Gets or sets the content to display inside the badge.
+	/// </summary>
+	[Parameter] public object? Content { get; set; }
+
+	/// <summary>
 	/// Gets or sets the CSS class names for the badge slots.
 	/// </summary>
 	[Parameter] public BadgeSlots? Classes { get; set; }
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="LumexBadge"/>.
+	/// </summary>
+	public LumexBadge()
+	{
+		As = "span";
+	}
+
+	[ExcludeFromCodeCoverage]
+	private string? GetStyles( string slot )
+	{
+		if( !_slots.TryGetValue( slot, out var styles ) )
+		{
+			throw new NotImplementedException();
+		}
+
+		return slot switch
+		{
+			nameof( BadgeSlots.Base ) => styles( Classes?.Base, Class ),
+			nameof( BadgeSlots.Badge ) => styles( Classes?.Badge ),
+			_ => throw new NotImplementedException()
+		};
+	}
 }
