f7bf62d5ee86c000ff591593cb03ef4e3dea24b2
desmondinho <daniil.norinn@gmail.com>
2025-07-10 19:04:58 +0300

    feat(badge): add badge slots

--- src/LumexUI/Components/Badge/LumexBadge.razor.cs
+++ src/LumexUI/Components/Badge/LumexBadge.razor.cs
@@ -2,12 +2,19 @@
 // LumexUI licenses this file to you under the MIT license
 // See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
 
+using LumexUI.Common;
+
+using Microsoft.AspNetCore.Components;
+
 namespace LumexUI;
 
 /// <summary>
 /// A component that represents a badge for displaying contextual information or status indicators.
 /// </summary>
-public partial class LumexBadge : LumexComponentBase
+public partial class LumexBadge : LumexComponentBase, ISlotComponent<BadgeSlots>
 {
-
+	/// <summary>
+	/// Gets or sets the CSS class names for the badge slots.
+	/// </summary>
+	[Parameter] public BadgeSlots? Classes { get; set; }
 }
