2bff4f94b29cf717f23c87dd258c8ef656616174
desmondinho <daniil.norinn@gmail.com>
2025-07-13 20:43:11 +0300

    feat(tooltip): add baseline implementation

--- src/LumexUI/Components/Tooltip/LumexTooltip.razor.cs
+++ src/LumexUI/Components/Tooltip/LumexTooltip.razor.cs
@@ -3,6 +3,7 @@
 // See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
 
 using LumexUI.Common;
+using LumexUI.Utilities;
 
 using Microsoft.AspNetCore.Components;
 
@@ -14,7 +15,24 @@ namespace LumexUI;
 public partial class LumexTooltip : LumexComponentBase, ISlotComponent<TooltipSlots>
 {
 	/// <summary>
+	/// Gets or sets the content around which the tooltip is rendered.
+	/// </summary>
+	[Parameter] public RenderFragment? ChildContent { get; set; }
+
+	/// <summary>
+	/// Gets or sets the content to display within the tooltip.
+	/// </summary>
+	[Parameter] public object? Content { get; set; }
+
+	/// <summary>
 	/// Gets or sets the CSS class names for the tooltip slots.
 	/// </summary>
 	[Parameter] public TooltipSlots? Classes { get; set; }
+
+	private readonly string _popoverId = Identifier.New();
+
+	private bool _isOpen;
+
+	private void OnPointerEnterAsync() => _isOpen = true;
+	private void OnPointerLeaveAsync() => _isOpen = false;
 }
