04b7b5a5b724dc00582ef8c2b8337a76b0823cd1
Denny09310 <k.denny2000@gmail.com>
2025-09-12 21:00:41 +0200

    feat: added initial component

---
+++ src/LumexUI/Components/Kbd/LumexKbd.razor.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) LumexUI 2024
+// LumexUI licenses this file to you under the MIT license
+// See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
+
+using LumexUI.Common;
+
+namespace LumexUI;
+
+/// <summary>
+/// <see cref="LumexKbd"/> is a component to display which key or combination of keys performs a given action.
+/// </summary>
+public partial class LumexKbd : LumexComponentBase, ISlotComponent<KbdSlots>
+{
+	/// <summary>
+	/// Gets or sets the CSS class names for the kbd slots.
+	/// </summary>
+	public KbdSlots? Classes { get; }
+}
