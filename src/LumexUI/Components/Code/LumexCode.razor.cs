415ae46a396032bb9fc6e51efc6a0781cbeac3eb
Denny09310 <k.denny2000@gmail.com>
2025-09-12 13:43:03 +0200

    feat: added 'Code' component

---
+++ src/LumexUI/Components/Code/LumexCode.razor.cs
@@ -0,0 +1,86 @@
+﻿// Copyright (c) LumexUI 2024
+// LumexUI licenses this file to you under the MIT license
+// See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
+
+using System.Diagnostics.CodeAnalysis;
+
+using LumexUI.Common;
+using LumexUI.Utilities;
+
+using Microsoft.AspNetCore.Components;
+
+namespace LumexUI;
+
+public partial class LumexCode : LumexComponentBase, ISlotComponent<CodeSlots>
+{
+	/// <summary>
+	/// Get or sets the content to be rendered inside the <see cref="LumexCode"/>.
+	/// </summary>
+	[Parameter] public RenderFragment? ChildContent { get; set; }
+
+	/// <summary>
+	/// Gets or sets the CSS class names for the code slots.
+	/// </summary>
+	[Parameter] public CodeSlots? Classes { get; set; }
+
+	/// <summary>
+	/// Gets or sets the color of the code.
+	/// </summary>
+	/// <remarks>
+	/// The default value is <see cref="ThemeColor.Default"/>.
+	/// </remarks>
+	[Parameter] public ThemeColor Color { get; set; } = ThemeColor.Default;
+
+	/// <summary>
+	/// Gets or sets the radius of the code.
+	/// </summary>
+	/// <remarks>
+	/// The default value is <see cref="Radius.Small"/>.
+	/// </remarks>
+	[Parameter] public Radius Radius { get; set; } = Radius.Small;
+
+	/// <summary>
+	/// Gets or sets the size of the code.
+	/// </summary>
+	/// <remarks>
+	/// The default value is <see cref="Size.Small"/>.
+	/// </remarks>
+	[Parameter] public Size Size { get; set; } = Size.Small;
+
+	private Dictionary<string, ComponentSlot> _slots = [];
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="LumexCode"/>.
+	/// </summary>
+	public LumexCode()
+	{
+		As = "code";
+	}
+
+	/// <inheritdoc />
+	protected override void OnParametersSet()
+	{
+		var code = Styles.Code.Style( TwMerge );
+		_slots = code( new()
+		{
+			[nameof( Color )] = Color.ToString(),
+			[nameof( Radius )] = Radius.ToString(),
+			[nameof( Size )] = Size.ToString(),
+		} );
+	}
+
+	[ExcludeFromCodeCoverage]
+	private string? GetStyles( string slot )
+	{
+		if( !_slots.TryGetValue( slot, out var styles ) )
+		{
+			throw new NotImplementedException();
+		}
+
+		return slot switch
+		{
+			nameof( CodeSlots.Base ) => styles( Classes?.Base ),
+			_ => throw new NotImplementedException()
+		};
+	}
+}
---
+++ src/LumexUI/Styles/Code.cs
@@ -0,0 +1,107 @@
+﻿// Copyright (c) LumexUI 2024
+// LumexUI licenses this file to you under the MIT license
+// See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
+
+using System.Diagnostics.CodeAnalysis;
+
+using LumexUI.Common;
+using LumexUI.Utilities;
+
+using TailwindMerge;
+
+namespace LumexUI.Styles;
+
+[ExcludeFromCodeCoverage]
+internal static class Code
+{
+	private static ComponentVariant? _variant;
+
+	public static ComponentVariant Style( TwMerge twMerge )
+	{
+		var twVariants = new TwVariants( twMerge );
+
+		return _variant ??= twVariants.Create( new VariantConfig()
+		{
+			Slots = new SlotCollection
+			{
+				[nameof( CodeSlots.Base )] = new ElementClass()
+					.Add( "px-2" )
+					.Add( "py-1" )
+					.Add( "h-fit" )
+					.Add( "font-mono" )
+					.Add( "font-normal" )
+					.Add( "inline-block" )
+					.Add( "whitespace-nowrap" )
+			},
+
+			Variants = new VariantCollection
+			{
+				[nameof( LumexCode.Color )] = new VariantValueCollection
+				{
+					[nameof( ThemeColor.Default )] = new SlotCollection
+					{
+						[nameof( CodeSlots.Base )] = ColorVariants.Flat[ThemeColor.Default]
+					},
+					[nameof( ThemeColor.Primary )] = new SlotCollection
+					{
+						[nameof( CodeSlots.Base )] = ColorVariants.Flat[ThemeColor.Primary]
+					},
+					[nameof( ThemeColor.Secondary )] = new SlotCollection
+					{
+						[nameof( CodeSlots.Base )] = ColorVariants.Flat[ThemeColor.Secondary]
+					},
+					[nameof( ThemeColor.Success )] = new SlotCollection
+					{
+						[nameof( CodeSlots.Base )] = ColorVariants.Flat[ThemeColor.Success]
+					},
+					[nameof( ThemeColor.Warning )] = new SlotCollection
+					{
+						[nameof( CodeSlots.Base )] = ColorVariants.Flat[ThemeColor.Warning]
+					},
+					[nameof( ThemeColor.Danger )] = new SlotCollection
+					{
+						[nameof( CodeSlots.Base )] = ColorVariants.Flat[ThemeColor.Danger]
+					},
+				},
+				[nameof( LumexCode.Size )] = new VariantValueCollection
+				{
+					[nameof( Size.Small )] = new SlotCollection
+					{
+						[nameof( CodeSlots.Base )] = "text-small",
+					},
+					[nameof( Size.Medium )] = new SlotCollection
+					{
+						[nameof( CodeSlots.Base )] = "text-medium",
+					},
+					[nameof( Size.Large )] = new SlotCollection
+					{
+						[nameof( CodeSlots.Base )] = "text-large",
+					}
+				},
+				[nameof( LumexCode.Radius )] = new VariantValueCollection
+				{
+					[nameof( Radius.None )] = new SlotCollection
+					{
+						[nameof( CodeSlots.Base )] = "rounded-none",
+					},
+					[nameof( Radius.Small )] = new SlotCollection
+					{
+						[nameof( CodeSlots.Base )] = "rounded-small",
+					},
+					[nameof( Radius.Medium )] = new SlotCollection
+					{
+						[nameof( CodeSlots.Base )] = "rounded-medium",
+					},
+					[nameof( Radius.Large )] = new SlotCollection
+					{
+						[nameof( CodeSlots.Base )] = "rounded-large",
+					},
+					[nameof( Radius.Full )] = new SlotCollection
+					{
+						[nameof( CodeSlots.Base )] = "rounded-full",
+					}
+				}
+			},
+		} );
+	}
+}
