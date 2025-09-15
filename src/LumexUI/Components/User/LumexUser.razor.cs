f11693d67e5f659c4b79b8a95a6871df50120785
Denny09310 <k.denny2000@gmail.com>
2025-09-15 09:35:19 +0200

    feat: added LumexUser initial styles

--- src/LumexUI/Components/User/LumexUser.razor.cs
+++ src/LumexUI/Components/User/LumexUser.razor.cs
@@ -2,12 +2,58 @@
 // LumexUI licenses this file to you under the MIT license
 // See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
 
+using System.Diagnostics.CodeAnalysis;
+
+using LumexUI.Common;
+using LumexUI.Utilities;
+
+using Microsoft.AspNetCore.Components;
+
 namespace LumexUI;
 
 /// <summary>
 /// A component that represents user information, such as an avatar, name, and email.
 /// </summary>
-public partial class LumexUser : LumexComponentBase
+public partial class LumexUser : LumexComponentBase, ISlotComponent<UserSlots>
 {
+	[Parameter] public string? Name { get; set; }
 
+	[Parameter] public string? Description { get; set; }
+
+	[Parameter] public bool IsFocusable { get; set; }
+
+	/// <summary>
+	/// Gets or sets the CSS class names for the user slots.
+	/// </summary>
+	[Parameter] public UserSlots? Classes { get; set; }
+
+	private Dictionary<string, ComponentSlot> _slots = [];
+
+	/// <inheritdoc/>
+	protected override void OnParametersSet()
+	{
+		var user = Styles.User.Style( TwMerge );
+		_slots = user( new()
+		{
+
+		} );
+	}
+
+	[ExcludeFromCodeCoverage]
+	private string? GetStyles( string slot )
+	{
+		if( !_slots.TryGetValue( slot, out var styles ) )
+		{
+			throw new NotImplementedException();
+		}
+
+		return slot switch
+		{
+			nameof( UserSlots.Base ) => styles( Classes?.Base, Class ),
+			nameof( UserSlots.Wrapper ) => styles( Classes?.Wrapper ),
+			nameof( UserSlots.Name ) => styles( Classes?.Name ),
+			nameof( UserSlots.Description ) => styles( Classes?.Description ),
+			_ => throw new NotImplementedException()
+		};
+	}
 }
---
+++ src/LumexUI/Styles/User.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) LumexUI 2024
+// LumexUI licenses this file to you under the MIT license
+// See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
+
+using System.Diagnostics.CodeAnalysis;
+
+using LumexUI.Components.User;
+using LumexUI.Utilities;
+
+using TailwindMerge;
+
+namespace LumexUI.Styles;
+
+[ExcludeFromCodeCoverage]
+internal static class User
+{
+	private static ComponentVariant? _variant;
+
+	public static ComponentVariant Style( TwMerge twMerge )
+	{
+		var twVariants = new TwVariants( twMerge );
+
+		return _variant ?? twVariants.Create( new VariantConfig()
+		{
+			Base = new ElementClass()
+				.Add( "inline-flex" )
+				.Add( "items-center" )
+				.Add( "justify-center" )
+				.Add( "gap-2" )
+				.Add( "rounded-small" )
+				.Add( "outline-solid" )
+				.Add( "outline-transparent" )
+				.Add( "outline-solid" )
+				.Add( "outline-transparent" )
+				.ToString(),
+
+			Slots = new SlotCollection
+			{
+				[nameof(UserSlots.Wrapper)] = "inline-flex flex-col items-start",
+
+				[nameof(UserSlots.Name)] = "text-small text-inherit",
+
+				[nameof(UserSlots.Description)] = "text-tiny text-foreground-400",
+			}
+		} );
+	}
+}
