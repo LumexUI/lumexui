292a0cd710c40a654d7f6bdcb187026ff6e602dd
Denny09310 <k.denny2000@gmail.com>
2025-09-15 09:09:23 +0200

    feat: added initial LumexUser component

---
+++ src/LumexUI/Components/User/LumexUser.razor.cs
@@ -0,0 +1,13 @@
+// Copyright (c) LumexUI 2024
+// LumexUI licenses this file to you under the MIT license
+// See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
+
+namespace LumexUI;
+
+/// <summary>
+/// A component that represents user information, such as an avatar, name, and email.
+/// </summary>
+public partial class LumexUser : LumexComponentBase
+{
+
+}
