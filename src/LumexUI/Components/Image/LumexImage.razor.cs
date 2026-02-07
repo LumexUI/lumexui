3017223e8028340405b06bdbdfe25990fdd20490
Kevin <venclovas.kevin@googlemail.com>
2026-02-07 23:20:32 +0100

    Add LumexImage component and refactor image usage
    Introduce LumexImage, a customizable Blazor image component supporting object fit, blur, radius, shadow, and more. Replace all <img> tags in demo components with <LumexImage> for consistent styling. Add supporting enums, style logic, and a basic render test. Update solution file accordingly.

---
+++ src/LumexUI/Components/Image/LumexImage.razor.cs
@@ -0,0 +1,96 @@
+﻿// Copyright (c) LumexUI 2024
+// LumexUI licenses this file to you under the MIT license
+// See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
+
+using LumexUI.Common;
+using LumexUI.Styles;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+using TailwindMerge;
+
+namespace LumexUI;
+
+/// <summary>
+/// A component representing a button.
+/// </summary>
+public partial class LumexImage : LumexComponentBase
+{
+	/// <summary>
+	/// Gets or sets the source of the image.
+	/// </summary>
+	[Parameter] public string Src { get; set; }
+
+	/// <summary>
+	/// Gets or sets the alt of the image.
+	/// </summary>
+	[Parameter] public string Alt { get; set; }
+
+	/// <summary>
+	/// Gets or sets the width of the image
+	/// </summary>
+	[Parameter] public int Width { get; set; }
+
+	/// <summary>
+	/// Gets or sets the height of the image
+	/// </summary>
+	[Parameter] public int Height { get; set; }
+
+	/// <summary>
+	/// Gets or sets the fit of the image
+	/// </summary>
+	[Parameter] public ObjectFit Fit { get; set; }
+
+	/// <summary>
+	/// Gets or sets a value indicating whether the image is full-width.
+	/// </summary>
+	[Parameter] public bool FullWidth { get; set; }
+
+	/// <summary>
+	/// Gets or sets a value indicating whether the image is full-height.
+	/// </summary>
+	[Parameter] public bool FullHeight { get; set; }
+
+	/// <summary>
+	/// Gets or sets the shadow of the image.
+	/// </summary>
+	/// <remarks>
+	/// Default value is <see cref="Shadow.Small"/>
+	/// </remarks>
+	[Parameter] public Shadow Shadow { get; set; } = Shadow.Small;
+
+	/// <summary>
+	/// Gets or sets a value indicating whether the image is blurred.
+	/// </summary>
+	[Parameter] public Blur Blur { get; set; } = Blur.None;
+
+	/// <summary>
+	/// Gets or sets the radius of the button.
+	/// </summary>
+	/// <remarks>
+	/// Default value is <see cref="Radius.Medium"/>
+	/// </remarks>
+	[Parameter] public Radius Radius { get; set; } = Radius.Medium;
+
+	/// <summary>
+	/// Gets or sets a callback that is fired whenever the button is clicked.
+	/// </summary>
+	[Parameter] public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+	private protected override string? RootClass =>
+		TwMerge.Merge( Image.GetStyles( this ) );
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="LumexButton"/>.
+	/// </summary>
+	public LumexImage()
+	{
+		As = "img";
+	}
+
+	private protected virtual Task OnClickAsync( MouseEventArgs args )
+	{
+		return OnClick.InvokeAsync( args );
+	}
+}
---
+++ tests/LumexUI.Tests/Components/Image/ImageTests.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) LumexUI 2024
+// LumexUI licenses this file to you under the MIT license
+// See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
+
+using Microsoft.Extensions.DependencyInjection;
+
+using TailwindMerge;
+
+namespace LumexUI.Tests.Components;
+public class ImageTests : TestContext
+{
+	public ImageTests()
+	{
+		Services.AddSingleton<TwMerge>();
+	}
+
+	[Fact]
+	public void Button_ShouldRenderCorrectly()
+	{
+		var action = () => RenderComponent<LumexImage>();
+
+		action.Should().NotThrow();
+	}
+}
